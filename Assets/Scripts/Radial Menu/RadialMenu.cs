a7f2a9b5020ee10d7f490509207ed2ca29ae9d78
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2022-02-16 16:50:49 -0600

    Basic radial menu element click interaction
    Clicking on the radial menu items now rotates the menu parent so the selected item sits at the top with some basic rotation, scale and color change animation

--- Assets/Scripts/Radial Menu/RadialMenu.cs
+++ Assets/Scripts/Radial Menu/RadialMenu.cs
@@ -1,22 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RadialMenu : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] RadialMenuElement[] _elements;
     [SerializeField] RadialMenuFraction _radialMenuFractionPrefab;
+    [SerializeField] int _firstSelected = 0;
 
     [Header("Appearance")]
     [SerializeField] float _gap = 1f;
     [Range(0, 1)] [SerializeField] float _iconDistance = 0.1f;
     [SerializeField] float _iconScale = 1f;
 
-    protected RadialMenuFraction[] _fractions;
+    [Header("Animation")]
+    [SerializeField] AnimationCurve _rotationEase;
 
+    [Header("References")]
+    [SerializeField] Transform _elementParent;
+
+    [SerializeField] RadialMenuFraction[] _fractions;
+
+    float _stepAngle;
     private void Start()
     {
         //BuildMenu();
+        _stepAngle = 360f / _elements.Length;
+
+        for (int i = 0; i < _fractions.Length; i++)
+        {
+            //It's important to use an auxiliary variable that gets created on the loop
+            //This allows the callback have access to the value from memory
+            int aux = i;
+
+            _fractions[i].Button.onClick.AddListener(() => {
+                //Debug.Log(aux);
+                RotateElementParent(-(aux - 1) * _stepAngle);
+
+                for (int j = 0; j < _fractions.Length; j++)
+                {
+                    _fractions[j].SetIsSelected(j == aux);
+                }
+            });
+        }
+
+        //Set the _firstSelected item in the list as selected
+        RotateElementParent(-(_firstSelected - 1) * _stepAngle);
+        _fractions[_firstSelected].SetIsSelected(true);
     }
 
     private void OnValidate()
@@ -45,11 +77,9 @@ public class RadialMenu : MonoBehaviour
 
         var stepAngle = 360f / _elements.Length;
 
-        Debug.Log(stepAngle);
-
         for (int i = 0; i < _elements.Length; i++)
         {
-            _fractions[i] = Instantiate(_radialMenuFractionPrefab, transform);
+            _fractions[i] = Instantiate(_radialMenuFractionPrefab, _elementParent);
 
             SetFractionAppereance(_fractions[i], _elements[i], i, stepAngle);
         }
@@ -64,7 +94,7 @@ public class RadialMenu : MonoBehaviour
         fraction.Circle.fillAmount = 1f / _elements.Length - _gap / 360f;
         fraction.Circle.transform.localPosition = Vector3.zero;
         fraction.Circle.transform.localRotation = Quaternion.Euler(0, 0, -stepAngle / 2f + _gap / 2f + index * stepAngle);
-        fraction.Circle.color = Color.gray;
+        //fraction.Circle.color = Color.gray;
 
         //Debug.Log(_fractions[i].CircleRect.rect.height);
 
@@ -72,11 +102,18 @@ public class RadialMenu : MonoBehaviour
         fraction.Icon.transform.localRotation = Quaternion.Euler(0, 0, (index - 1) * stepAngle + 90);
         fraction.Icon.transform.localScale = Vector3.one * _iconScale;
 
+        //temp
+        fraction.Name.text = index.ToString();
 
         if (element == null)
             return;
 
         fraction.Icon.sprite = element.Icon;
-        fraction.Name.text = element.Name;
+        //fraction.Name.text = element.Name;
+    }
+
+    void RotateElementParent(float targetAngle)
+    {
+        _elementParent.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, targetAngle), 0.3f).SetUpdate(true).SetEase(_rotationEase);
     }
 }
--- Assets/Scripts/Radial Menu/RadialMenuFraction.cs
+++ Assets/Scripts/Radial Menu/RadialMenuFraction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class RadialMenuFraction : MonoBehaviour
 {
@@ -9,10 +10,36 @@ public class RadialMenuFraction : MonoBehaviour
     public Image Circle;
     public RectTransform CircleRect;
     public Text Name;
+    public Button Button;
+
+    [Header("Appereance")]
+    [SerializeField] Color _selectedColor = Color.white;
+    [SerializeField] Color _unSelectedColor = Color.gray;
+    [SerializeField] float _selectedTargetScale = 1.05f;
+
+    float _originalCircleScale = 1;
+
+    bool _isSelected = false;
+
+    private void Start()
+    {
+        Circle.color = _unSelectedColor;
+
+        _originalCircleScale = Circle.transform.localScale.x;
+    }
 
     private void OnValidate()
     {
         if (Circle != null)
             CircleRect = Circle.GetComponent<RectTransform>();
+    }
+
+    public void SetIsSelected(bool value)
+    {
+        _isSelected = value;
+
+        Circle.DOColor(_isSelected ? _selectedColor : _unSelectedColor, 0.2f);
+
+        Circle.transform.DOScale(Vector3.one * (_isSelected ? _selectedTargetScale : _originalCircleScale), 0.2f);
     }
 }
